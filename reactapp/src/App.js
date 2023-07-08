63c0ce54a7f686f8e5fd53118bedf9ee94704b73
ras Bamba <b24media.info@gmail.com>
2023-07-08 15:08:22 -0700

    theme added componenent

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,10 +1,22 @@
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline,<ThemeProvider theme={}>
+  
+</ThemeProvider>import { ThemeProvider } from "@emotion/react";
+ } from "@mui/material";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
+    <ColorModeContext.Provider value = {colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        </ThemeProvider>
+
     <div className="App">
-      
+      <main className = "content"></main>
     </div>
+    <ThemeProvider/>
+    </ColorModeContext.Provider>
   );
 }
 
